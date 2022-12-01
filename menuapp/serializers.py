2d506db049c3f0ff0b2a37c261ae2dda0312910b
Vincent Suryakim <vincentsuryakim@gmail.com>
2022-12-01 14:44:26 +0700

    fix(menu): refactor code

--- menuapp/serializers.py
+++ menuapp/serializers.py
@@ -4,11 +4,15 @@ from .models import Menu
 from bookantinauth.serializers import SellerMenuSerializer
 
 class MenuSerializer(serializers.ModelSerializer):
-    seller = SellerMenuSerializer()
-
     class Meta:
         model = Menu
         fields = ('id', 'name', 'price', 'seller', 'type')
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['seller'] = SellerMenuSerializer(instance.seller).data
+
+        return representation
 
 class MenuOnlySerializer(serializers.ModelSerializer):
     class Meta:
