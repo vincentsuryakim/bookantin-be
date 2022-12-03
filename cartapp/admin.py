aa4874697e7bd25bb50c9f2618eecefee789e6ce
gbhisma <bhismagregorius@gmail.com>
2022-12-03 10:26:16 +0700

    fixed and registered models

--- cartapp/admin.py
+++ cartapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from cartapp.models import Cart, CartContent
+
 # Register your models here.
+admin.site.register(Cart)
+admin.site.register(CartContent)
--- cartapp/models.py
+++ cartapp/models.py
@@ -1,14 +1,20 @@
 from django.db import models
-from rest_framework.authtoken.admin import User
+from django.conf import settings
 
 
 # Create your models here.
 class Cart(models.Model):
-    checkedOut = models.BooleanField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart', default=False)
-    # status = models.
+    class CartStatus(models.IntegerChoices):
+        AWAITING_PAYMENT = 1
+        PROCESSED = 2
+        READY_FOR_PICKUP = 3
+
+    checkedOut = models.BooleanField(default=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='cart', default=False)
+    status = models.IntegerField(choices=CartStatus.choices, default=1)
 
 
 class CartContent(models.Model):
+    # Many to Many between cart and menu
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='contents')
-    # menu = models.ForeignKey(Menu, on_delete=models.CASCADE(, related_name='menu'))
+    # menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='menu'))
