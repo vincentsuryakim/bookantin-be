458edf6afab17f9597bb7984e24eb58d2ac8de06
gbhisma <bhismagregorius@gmail.com>
2022-12-03 10:36:49 +0700

    added date fields on Cart model

--- cartapp/models.py
+++ cartapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 
+from menuapp.models import Menu
+
 
 # Create your models here.
 class Cart(models.Model):
@@ -12,9 +14,13 @@ class Cart(models.Model):
     checkedOut = models.BooleanField(default=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='cart', default=False)
     status = models.IntegerField(choices=CartStatus.choices, default=1)
+    checkedOutTime = models.DateTimeField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    lastUpdated = models.DateTimeField(auto_now=True)
 
 
 class CartContent(models.Model):
     # Many to Many between cart and menu
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='contents')
-    # menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='menu'))
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='menu')
+    currentPrice = models.IntegerField()
